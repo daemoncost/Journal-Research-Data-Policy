ba2a4cb93788d38ea1ce12a1d1d2c59c12f84954
ppegolo <ppegolo@sissa.it>
2024-09-08 11:40:13 +0200

    Rename and update load_data function

--- src/daemon_analysis_tools/data_processing.py
+++ src/daemon_analysis_tools/data_processing.py
@@ -1,9 +1,9 @@
 import re
-
 import pandas as pd
+from daemon_analysis_tools.data.question import Question
 
 
-def load_and_process_data(file_path):
+def load_and_process_csv(file_path):
     data = pd.read_csv(file_path)
     try:
         data.drop(["Zeitstempel", "E-Mail-Adresse", "Punkte"], axis=1, inplace=True)
@@ -12,14 +12,6 @@ def load_and_process_data(file_path):
     except KeyError:
         pass
         # Already preprocessed to remove E-mail addresses
-
-    keys = data.keys()
-    keys = [
-        key
-        for key in keys
-        if "Please add the text from the RDP that supports your answer below:" in key
-    ]
-    data.drop(keys, axis=1, inplace=True)
 
     data.rename(
         columns={
@@ -38,6 +30,11 @@ def load_and_process_data(file_path):
     data_duplicated["Publisher Name"] = data_duplicated["Publisher Name"].apply(
         normalize_publisher
     )
+
+    # Normalize the journal names
+    # with open("../data/journal_normalizer.yaml", "r") as file:
+    # normalizazion_dict = yaml.safe_load(file)
+    data_duplicated["journal"] = data_duplicated["journal"].apply(normalize_journal)
 
     return data_duplicated
 
@@ -97,10 +94,91 @@ def normalize_publisher(name):
     return normalization_dict.get(name, name)
 
 
+# Normalize journal names
+def normalize_journal(name):
+    name = clean_journal_name(name)
+    normalization_dict = {
+        (
+            "accounts_of_materials_research_-_acc_mater_res"
+        ): "accounts_of_materials_research",
+        (
+            "acs_applied_materials_and_interfaces_-_" "acs_appl_mater_interfaces"
+        ): "applied_materials_and_interfaces",
+        (
+            "environmental_science_and_technology_-_environ_sci_technol"
+        ): "environmental_science_and_technology",
+        (
+            "the_journal_of_physical_chemistry_c_-_j_phys_chem_c"
+        ): "the_journal_of_physical_chemistry_c",
+        (
+            "https//publishingaiporg/resources/researchers/"
+            "open-science/research-data-policy/"
+        ): None,
+        "dalton-transactions": "dalton_transactions",
+        "nanoscale-advances": "nanoscale_advances",
+        "nature_eniergy": "nature_energy",
+        "chemistry_-_a_european_journal": "chemistry_a_european_journal",
+        "ce/paper": "ce-papers",
+        "ce/papers": "ce-papers",
+    }
+    return normalization_dict.get(name, name)
+
+
 # Function to clean journal names for file naming
 def clean_journal_name(journal_name):
+    # journal_name = journal_name.split("-")[0]
+    journal_name = journal_name.strip()
+    journal_name = re.sub(r"\n", "", journal_name)
     journal_name = journal_name.replace("&", "and")
     journal_name = re.sub(r"[.?:]", "", journal_name)
     journal_name = journal_name.replace(" ", "_")
     journal_name = journal_name.lower()
     return journal_name
+
+
+def group_questions_by_journal(data):
+    grouped_data = data.groupby(["Publisher Name", "journal"])
+    grouped_questions = {}
+
+    for (publisher_name, journal_name), group in grouped_data:
+        if publisher_name not in grouped_questions:
+            grouped_questions[publisher_name] = {}
+
+        if journal_name not in grouped_questions[publisher_name]:
+            grouped_questions[publisher_name][journal_name] = {}
+
+        for i, column in enumerate(data.columns):
+            if column.split(".")[
+                0
+            ].isdigit():  # Check if the column label starts with an integer
+                q_num = int(column.split(".")[0])
+
+                # Check for the presence of an explanation column
+                next_column = data.columns[i + 1] if i + 1 < len(data.columns) else None
+                explanation_col = None
+                if (
+                    next_column
+                    and (
+                        "Please add the text from the RDP "
+                        "that supports your answer below:"
+                    )
+                    in next_column
+                ):
+                    explanation_col = next_column
+
+                if q_num not in grouped_questions[publisher_name][journal_name]:
+                    grouped_questions[publisher_name][journal_name][q_num] = Question(
+                        column
+                    )
+
+                for idx, answer in enumerate(group[column]):
+                    explanation = (
+                        group[explanation_col].iloc[idx]
+                        if explanation_col in group.columns
+                        else ""
+                    )
+                    grouped_questions[publisher_name][journal_name][q_num].add_answer(
+                        answer, explanation
+                    )
+
+    return grouped_questions
