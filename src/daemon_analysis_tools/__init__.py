6fb88fba52409603edf831e22a98dff29640afc4
NatalyLo <lopanitsyna.nataliya@gmail.com>
2025-03-15 16:29:57 +0100

    moved all facade functions to init

--- src/daemon_analysis_tools/__init__.py
+++ src/daemon_analysis_tools/__init__.py
@@ -1,1 +1,13 @@
-from .facade_functions import *
+"""This module provides the main functions intended for public use."""
+
+from daemon_analysis_tools.io.csv_handler import load_and_process_csv
+from daemon_analysis_tools.io.yaml_handler import save_answers_to_yaml
+from daemon_analysis_tools.processing.grouper import group_questions_by_journal
+from daemon_analysis_tools.services.discrepancy_resolver import resolve_discrepancy
+
+__all__ = [
+    "load_and_process_csv",
+    "group_questions_by_journal",
+    "resolve_discrepancy",
+    "save_answers_to_yaml",
+]
