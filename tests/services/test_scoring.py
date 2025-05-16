d9b3ade23e3e52128ab750e035421d4f2d293e82
lukashoermann <lukashoermann2000@gmail.com>
2025-05-16 16:47:44 +0100

    added test for services/scoring.py

---
+++ tests/services/test_scoring.py
@@ -0,0 +1,57 @@
+import unittest
+
+from daemon_analysis_tools.datamodels.question import Question
+from daemon_analysis_tools.io.yaml_base_handler import load_yaml
+from daemon_analysis_tools.services.scoring import (
+    _get_maximum_possible_score,
+    _get_question_type_lookup,
+    get_journal_score,
+    get_question_score,
+)
+
+
+class TestScoring(unittest.TestCase):
+    def setUp(self):
+        self.question_type = load_yaml("../../data/metadata/question_type.yaml")
+        self.multiple_choice_scores = load_yaml(
+            "../../data/metadata/question_metadata_score.yaml"
+        )
+
+        # Create a dummy Question with one answer.
+        self.question = Question(
+            question_id="rdp_exist",
+            text="1. Existence of research data policy",
+            is_open=False,
+        )
+        self.question._add_answer("research data policy (rdp) exists.", "Explanation A")
+        self.question._set_correct_answer(self.question.answers[0])
+
+        # Create a dummy Journal with one question.
+        self.journal = {"rdp_exist": self.question}
+
+    def test_get_maximum_possible_score(self):
+        max_score = _get_maximum_possible_score(self.multiple_choice_scores)
+
+        self.assertEqual(max_score, 19.0)
+
+    def test_get_question_score(self):
+        question_number_lookup = _get_question_type_lookup(self.multiple_choice_scores)
+
+        score = get_question_score(
+            self.question.question_id,
+            self.question,
+            self.multiple_choice_scores,
+            self.question_type,
+            question_number_lookup,
+        )
+
+        self.assertEqual(score, 1.0)
+
+    def test_get_journal_score(self):
+        score = get_journal_score(self.journal)
+
+        self.assertAlmostEqual(score, 1.0 / 19.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
